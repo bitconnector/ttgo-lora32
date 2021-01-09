2e006457b03f1e767ea143dfe43f716d516fe6a3
bitconnector <luk-krat@web.de>
2021-01-09 13:07:34 +0100

    first setup

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("startup");
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
