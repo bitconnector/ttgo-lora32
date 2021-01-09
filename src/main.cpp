85bba8865d436d44b0e295cb6446552e7363e826
bitconnector <luk-krat@web.de>
2021-01-09 14:21:14 +0100

    added oled

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,90 @@
-/*********
-  Modified from the examples of the Arduino LoRa library
-  More resources: https://randomnerdtutorials.com
-*********/
+#include <Arduino.h>
 
 #include <SPI.h>
 #include <LoRa.h>
 
-//define the pins used by the transceiver module
-#define ss 18
-#define rst 14
-#define dio0 26
+// LoRa pins
+#define LORA_MISO 19
+#define LORA_CS 18
+#define LORA_MOSI 27
+#define LORA_SCK 5
+#define LORA_RST 14
+#define LORA_IRQ 26
 
-void setup() {
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+// Define OLED PIN
+#define OLED_SDA 4
+#define OLED_SCL 15
+#define OLED_RST 16
+
+Adafruit_SSD1306 display(128, 32, &Wire, OLED_RST);
+
+
+void setup()
+{
   //initialize Serial Monitor
   Serial.begin(115200);
-  while (!Serial);
+  //while (!Serial);
   Serial.println("LoRa Receiver");
 
+  Wire.begin(OLED_SDA, OLED_SCL);
+
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+  { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;)
+      ; // Don't proceed, loop forever
+  }
+
+  Serial.println("Display initialized");
+  display.clearDisplay();
+
+  display.setTextColor(WHITE);
+  display.setTextSize(1);
+  display.setCursor(0, 0);
+  display.println("Welcome to LORA");
+
+  display.setTextSize(1);
+  display.println("Lora sender");
+  display.display();
+
   //setup LoRa transceiver module
-  LoRa.setPins(ss, rst, dio0);
-  
-  //replace the LoRa.begin(---E-) argument with your location's frequency 
+  SPI.begin(LORA_SCK, LORA_MISO, LORA_MOSI, LORA_CS);
+  LoRa.setPins(LORA_CS, LORA_RST, LORA_IRQ);
+
+  //replace the LoRa.begin(---E-) argument with your location's frequency
   //433E6 for Asia
   //866E6 for Europe
   //915E6 for North America
-  while (!LoRa.begin(866E6)) {
+  while (!LoRa.begin(866E6))
+  {
     Serial.println(".");
     delay(500);
   }
-   // Change sync word (0xF3) to match the receiver
+  // Change sync word (0xF3) to match the receiver
   // The sync word assures you don't get LoRa messages from other LoRa transceivers
   // ranges from 0-0xFF
   LoRa.setSyncWord(0xF3);
   Serial.println("LoRa Initializing OK!");
 }
 
-void loop() {
+void loop()
+{
   // try to parse packet
   int packetSize = LoRa.parsePacket();
-  if (packetSize) {
+  if (packetSize)
+  {
     // received a packet
     Serial.print("Received packet '");
 
     // read packet
-    while (LoRa.available()) {
+    while (LoRa.available())
+    {
       String LoRaData = LoRa.readString();
-      Serial.print(LoRaData); 
+      Serial.print(LoRaData);
     }
 
     // print RSSI of packet
