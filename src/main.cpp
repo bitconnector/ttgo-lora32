f95d3142d71ce793a0dc468334b7d71e165f0614
bitconnector <luk-krat@web.de>
2021-01-09 17:16:25 +0100

    finished receiving role with reply

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 
+#define LED 2
+
 #include <SPI.h>
 #include <LoRa.h>
 
@@ -22,9 +24,10 @@
 
 Adafruit_SSD1306 display(128, 64, &Wire, OLED_RST);
 
-
 void setup()
 {
+
+  pinMode(LED, OUTPUT);
   //initialize Serial Monitor
   Serial.begin(115200);
   Serial.println("starting up...");
@@ -65,24 +68,171 @@ void setup()
   Serial.println("LoRa Initializing OK!");
 }
 
+void blinkLED(unsigned long duration);
+void updateLED();
+
+void displayTimestamp();
+void parsePacket();
+bool Button = 1;
+unsigned long ButtonTime;
+
+unsigned long Time1, Timed;
+bool role = 0;
+int param = 0;
+
+int counter;
+
+String RecivedData;
+unsigned long RecivedDataTime;
+
 void loop()
 {
-  // try to parse packet
-  int packetSize = LoRa.parsePacket();
-  if (packetSize)
-  {
-    // received a packet
-    Serial.print("Received packet '");
 
-    // read packet
-    while (LoRa.available())
+  if (Button != digitalRead(0))
+  {
+    //Serial.print("Button ");
+    //Serial.println(Button);
+    Button = digitalRead(0);
+    if (Button)
     {
-      String LoRaData = LoRa.readString();
-      Serial.print(LoRaData);
+      if ((ButtonTime + 1000) < millis())
+      { //pressed for 1s
+        role = !role;
+        param = 0;
+      }
+      else
+      {
+        param++;
+        if (param > 9)
+          param = 0;
+      }
+    }
+    ButtonTime = millis();
+  }
+
+  if (millis() - Time1 > 100)
+  {
+    Timed = millis();
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.print("Ro: ");
+    if (role)
+    {
+      display.print("send ");
+      display.print(param);
+      display.print("   ");
+      displayTimestamp();
+      display.println();
     }
 
-    // print RSSI of packet
-    Serial.print("' with RSSI ");
-    Serial.println(LoRa.packetRssi());
+    else
+    {
+      display.print("empf ");
+      display.print(param);
+      display.print("   ");
+      displayTimestamp();
+      display.println();
+      display.print("last: ");
+      display.print(LoRa.packetRssi());
+      display.print(" @");
+      float lastSeen = (millis() - RecivedDataTime);
+      display.print(lastSeen / 1000);
+      display.println("s");
+      display.println("Data:");
+      display.print(RecivedData);
+    }
+
+    display.display();
   }
+
+  if (role)
+  {
+    if (millis() - Time1 > 1000)
+    {
+      Time1 = millis();
+      LoRa.beginPacket();
+      LoRa.print(counter);
+      LoRa.endPacket();
+      counter++;
+    }
+    // try to parse packet
+    parsePacket();
+  }
+  else
+  {
+    int packetSize = LoRa.parsePacket();
+    if (packetSize != 0)
+    {
+      blinkLED(200);
+      RecivedDataTime = millis();
+      Serial.print("Received packet '");
+      while (LoRa.available())
+      {
+        RecivedData = LoRa.readString();
+        Serial.print(RecivedData);
+      }
+      Serial.print("' with RSSI ");
+      Serial.println(LoRa.packetRssi());
+      if (param == 1)
+      {
+        LoRa.beginPacket();
+        LoRa.print(counter);
+        LoRa.endPacket();
+        Serial.print("Replied: ");
+        Serial.print(counter);
+        Serial.println();
+        counter++;
+      }
+    }
+  }
+  updateLED();
+}
+
+void parsePacket()
+{
+  int packetSize = LoRa.parsePacket();
+  if (packetSize == 0)
+    return;
+  Serial.print("Received packet '");
+  while (LoRa.available())
+  {
+    RecivedData = LoRa.readString();
+    Serial.print(RecivedData);
+  }
+  Serial.print("' with RSSI ");
+  Serial.println(LoRa.packetRssi());
+}
+
+void displayTimestamp()
+{
+  uint32_t seconds = millis() / 1000;
+  uint32_t minutes = seconds / 60;
+  seconds %= 60;
+  uint32_t hours = minutes / 60;
+  minutes %= 60;
+  if (hours < 10)
+    display.print("0");
+  display.print(hours);
+  display.print(":");
+  if (minutes < 10)
+    display.print("0");
+  display.print(minutes);
+  display.print(":");
+  if (seconds < 10)
+    display.print("0");
+  display.print(seconds);
+}
+
+unsigned long LEDIntervall;
+
+void blinkLED(unsigned long duration)
+{
+  digitalWrite(LED, HIGH);
+  LEDIntervall = millis() + duration;
+}
+
+void updateLED()
+{
+  if (LEDIntervall < millis())
+    digitalWrite(LED, LOW);
 }
